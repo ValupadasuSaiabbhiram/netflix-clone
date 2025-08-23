0002fc01b14db90fc2dce010e8d7265ccc26c4a8
sai abbhiram <valupadasusaiabbhiram@gmail.com>
2025-08-23 23:02:37 +0530

    feat: add protected authCheck endpoint

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import { signup } from '../controllers/auth.controller.js';
+import { authCheck, signup } from '../controllers/auth.controller.js';
 import { login } from '../controllers/auth.controller.js';
 import { logout } from '../controllers/auth.controller.js';
+import { protectRoute } from '../middleware/protectRoute.js';
 
 
 const router = express.Router();
@@ -11,6 +12,8 @@ router.post('/signup', signup);
 router.post('/login', login);
 
 router.post('/logout', logout);
+
+router.get('/authCheck', protectRoute,  authCheck);
 
 
 
