bb63c66726c8dae77dd67fb5a710233f3962b6cc
sai abbhiram <valupadasusaiabbhiram@gmail.com>
2025-08-23 23:06:26 +0530

    feat: integrate auth store and email prefill in signup page

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,16 +1,21 @@
-
 import { useState } from 'react';
 import {Link} from 'react-router-dom';
+import { useAuthStore } from "../store/authUser";
 
 const SignUpPage = () => {
+    const { searchParams } = new URL(document.location)
+    const emailValue = searchParams.get("email")
 
-    const [email, setEmail] = useState('');
+    const [email, setEmail] = useState(emailValue || "");
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
+    const { signup, isSigningUp } = useAuthStore();
+
+
     const handleSignUp = (e) => {
         e.preventDeafult();
-        console.log(email, username, password);
+        signup({email, username, password})
 
 
     }
@@ -60,8 +65,11 @@ const SignUpPage = () => {
           />
         </div>
 
-        <button className='w-full py-2 bg-red-600 text-white font-semibold rounded-md hover:bg-red-700'>
-            Sign Up
+        <button className='w-full py-2 bg-red-600 text-white font-semibold rounded-md hover:bg-red-700'
+          disabled={isSigningUp}
+        >
+          {isSigningUp ? "Loading..." : "Sign Up"}
+
         </button>
         </form>
         <div className ="text-center text-gray-400">
